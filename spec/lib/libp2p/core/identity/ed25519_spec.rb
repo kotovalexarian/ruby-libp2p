632639a4c5c8a523af015e4c274c7499b00e4418
Alex Kotov <kotovalexarian@gmail.com>
2021-07-02 15:30:05 +0500

    Organize spec

--- spec/lib/libp2p/core/identity/ed25519_spec.rb
+++ spec/lib/libp2p/core/identity/ed25519_spec.rb
@@ -7,35 +7,8 @@ RSpec.describe Libp2p::Core::Identity::Ed25519 do
   let(:public_key) { private_key.public_key }
 
   let(:message) { 'Hello, World!' }
-  let(:other_message) { 'Hi, Universe!' }
 
   let(:signature) { private_key.sign message }
-
-  let :other_signature do
-    other_signature = signature.dup
-    index = rand other_signature.length
-
-    other_signature[index] =
-      if other_signature[index] == "\0"
-        "\1"
-      else
-        (other_signature[index].ord - 1).chr
-      end
-
-    other_signature
-  end
-
-  specify do
-    expect(public_key.verify(message, signature)).to equal true
-  end
-
-  specify do
-    expect(public_key.verify(other_message, signature)).to equal false
-  end
-
-  specify do
-    expect(public_key.verify(message, other_signature)).to equal false
-  end
 
   specify do
     expect { described_class::PrivateKey.new 123 }.to raise_error(
@@ -49,5 +22,37 @@ RSpec.describe Libp2p::Core::Identity::Ed25519 do
       TypeError,
       "Expected #{::Ed25519::VerifyKey}, got #{Integer}",
     )
+  end
+
+  specify do
+    expect(public_key.verify(message, signature)).to equal true
+  end
+
+  context 'with other message' do
+    let(:other_message) { 'Hi, Universe!' }
+
+    specify do
+      expect(public_key.verify(other_message, signature)).to equal false
+    end
+  end
+
+  context 'with other signature' do
+    let :other_signature do
+      other_signature = signature.dup
+      index = rand other_signature.length
+
+      other_signature[index] =
+        if other_signature[index] == "\0"
+          "\1"
+        else
+          (other_signature[index].ord - 1).chr
+        end
+
+      other_signature
+    end
+
+    specify do
+      expect(public_key.verify(message, other_signature)).to equal false
+    end
   end
 end
